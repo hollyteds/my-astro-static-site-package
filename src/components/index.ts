64f3431c00330965352e62ffc2ccfaf8bac89273
hori-ak <t-horiuchi@aoba-koukoku.co.jp>
2025-07-15 11:28:54 +0900

    コンポーネントのエクスポートを index.ts に集約

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// common ディレクトリ
+export { default as Header } from '@components/common/Header.astro';
+export { default as Footer } from '@components/common/Footer.astro';
+export { default as Headline } from '@components/common/Headline.astro';
+export { default as Button } from '@components/common/Button.astro';
+export { default as InnerInSection } from '@components/common/InnerInSection.astro';
